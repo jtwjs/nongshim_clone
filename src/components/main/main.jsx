666d887eabb939f0e2c7ba280405b38fe0ebd177
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-02-10 13:21:47 +0900

    add stroy components

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,6 +3,7 @@ import Home from '../home/home';
 import News from '../news/news';
 import Intro from '../intro/intro';
 import styles from './main.module.css';
+import Story from '../story/story';
 
 const Main = memo(() => {
   return (
@@ -10,6 +11,7 @@ const Main = memo(() => {
       <Home/>
       <News/>
       <Intro/>
+      <Story/>
     </main>
   );
 
